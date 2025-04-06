ccc4948136287a5145ddd084c47db938eacba405
Shanmukh459 <saikrrish459@gmail.com>
2025-04-06 14:37:38 -0700

    update a genre

--- index.js
+++ index.js
@@ -16,17 +16,14 @@ app.get("/api/genres", (req, res) => {
 app.get("/api/genres/:id", (req, res) => {
   const genre = genres.find((g) => g.id === parseInt(req.params.id))
 
-  if (!genre) return res.status(400).send("Genre with given ID doesn't exist.")
+  if (!genre) return res.status(404).send("Genre with given ID doesn't exist.")
 
   res.send(genre)
 })
 
 app.post("/api/genres", (req, res) => {
-  const schema = {
-    name: Joi.string().min(3).required(),
-  }
-  const result = Joi.validate(req.body, schema)
-  if (result.error) return res.status(400).send(result.error.details[0].message)
+  const { error } = validateGenre(req.body)
+  if (error) return res.status(400).send(error.details[0].message)
 
   const genre = {
     id: genres.length + 1,
@@ -35,6 +32,24 @@ app.post("/api/genres", (req, res) => {
   genres.push(genre)
   res.send(genre)
 })
+
+app.put("/api/genres/:id", (req, res) => {
+  const { error } = validateGenre(req.body)
+  if (error) return res.status(400).send(error.details[0].message)
+
+  const genre = genres.find((g) => g.id === parseInt(req.params.id))
+  if (!genre) return res.status(404).send("Genre with given ID doesn't exist.")
+
+  genre.name = req.body.name
+  res.send(genre)
+})
+
+const validateGenre = (genre) => {
+  const schema = {
+    name: Joi.string().min(3).required(),
+  }
+  return Joi.validate(genre, schema)
+}
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on port ${port}...`))
