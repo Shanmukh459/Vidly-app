a799a2619486e0f00b45b0f578c1494718b508d3
Shanmukh459 <saikrrish459@gmail.com>
2025-04-26 17:41:41 -0700

    rest of the apis for customers endpoint

--- routes/customers.js
+++ routes/customers.js
@@ -28,6 +28,38 @@ router.post("/", async (req, res) => {
   res.send(customer)
 })
 
+router.put("/:id", async (req, res) => {
+  const { error } = validateCustomer(req.body)
+  if (error) return res.status(400).send(error.details[0].message)
+
+  const customer = await Customer.findByIdAndUpdate(
+    req.params.id,
+    { name: req.body.name, phone: req.body.phone, isGold: req.body.isGold },
+    { new: true }
+  )
+
+  if (!customer)
+    return res.status(404).send("Customer with given ID doesn't exist.")
+  res.send(customer)
+})
+
+router.delete("/:id", async (req, res) => {
+  const customer = await Customer.findByIdAndDelete(req.params.id)
+  if (!customer)
+    return res.status(404).send("Customer with given ID doesn't exist.")
+
+  res.send(customer)
+})
+
+router.get("/:id", async (res, req) => {
+  const customer = await Customer.findById(req.params.id)
+
+  if (!customer)
+    return res.status(400).send("Customer with given ID doesn't exist.")
+
+  res.send(customer)
+})
+
 const validateCustomer = (customer) => {
   const schema = {
     name: Joi.string().min(5).max(50).required(),
